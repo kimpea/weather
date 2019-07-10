f4c430d449fe5a2ab2f8ffedab70726d9d954580
kimpea <kim.pearton@live.co.uk>
2019-07-10 18:18:45 +0100

    Create components folder, create nav component

---
+++ weather-app/src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => (
+    <div className="Nav">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a className="navbar-brand" href="#">Weather App</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </div>
+);
+
+export default Nav;
